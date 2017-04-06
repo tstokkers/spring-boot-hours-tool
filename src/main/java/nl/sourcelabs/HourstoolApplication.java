2a3bf2970a39927b92e653bfba1ad6483b5ae85a
Tim Stokkers <tstokkers@gmail.com>
2017-04-06 12:05:18 +0200

    initial project setup

---
+++ src/main/java/nl/sourcelabs/HourstoolApplication.java
@@ -0,0 +1,13 @@
+package nl.sourcelabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HourstoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HourstoolApplication.class, args);
+		System.out.println("turd");
+	}
+}
---
+++ src/test/java/nl/sourcelabs/HourstoolApplicationTests.java
@@ -0,0 +1,16 @@
+package nl.sourcelabs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HourstoolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
